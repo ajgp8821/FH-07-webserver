269329aa18d7ea9e496de1e9150b85093021f308
ajgp8821 <ajgp8821@gmail.com>
2021-11-12 09:36:49 -0300

    Inicio con Express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Home Page')
+});
+
+app.get('*', (req, res) => {
+  // res.send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () =>{
+  console.log(`App listening at http://localhost:${port}`);
+});
+
